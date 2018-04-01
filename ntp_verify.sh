f93b206e462f066c93550a42c8d9282f664ffefd
alex bilyk <asbilyk@gmail.com>
2018-04-02 00:32:16 +0300

    add check

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if [-n `pgrep ntp`]
+if [[ -z "`pgrep ntp`" ]] 
+  then
+  sudo service ntp start
+fi
+if diff "/etc/ntp.conf" "$(pwd)/temp"
 then
-`service ntp start`
-fi 
-
+>&1
+server ua.pool.ntp.org > /etc/ntp.conf
+service ntp start
+rm $(pwd)/temp
+fi
